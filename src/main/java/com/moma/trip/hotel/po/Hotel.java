cf9a73128a12d89966f043e586d96b09b33a0486
simusco <simusco@163.com>
2015-09-25 14:20:53 +0800

    个性实体

--- src/main/java/com/moma/trip/hotel/po/Hotel.java
+++ src/main/java/com/moma/trip/hotel/po/Hotel.java
@@ -6,6 +6,37 @@ public class Hotel {
 	private String name;
 	private String description;
 	private String address;
-	
-	
+
+	public String getHotelId() {
+		return hotelId;
+	}
+
+	public void setHotelId(String hotelId) {
+		this.hotelId = hotelId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 }
--- src/main/java/com/moma/trip/hotel/po/HotelImage.java
+++ src/main/java/com/moma/trip/hotel/po/HotelImage.java
@@ -4,4 +4,13 @@ import com.moma.trip.image.po.Image;
 
 public class HotelImage extends Image {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3624352715858145262L;
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
 }
