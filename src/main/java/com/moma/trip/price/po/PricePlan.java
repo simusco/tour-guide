95206cb1257aef128811a747ebfc9bef788f94b0
simusco <simusco@163.com>
2015-09-25 14:18:47 +0800

    个性实体

--- src/main/java/com/moma/trip/price/po/PricePlan.java
+++ src/main/java/com/moma/trip/price/po/PricePlan.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class PricePlan implements Serializable{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2449141213452083589L;
 	private String pricePlanId;
 	private String ticketGroupId;
 	private String synchTime;// 同步时间
